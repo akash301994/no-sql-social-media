a1314e11af8916f47792823a6f3b191f66617c0a
akash301994 <patel.akash30@icloud.com>
2023-09-24 15:12:38 -0400

    creating models

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model, Types } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(
+          v
+        );
+      },
+    },
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+});
